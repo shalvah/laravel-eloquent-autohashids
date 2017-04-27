88603bba7dfdb46511a67b64eda2aa9f1bb6be53
Shalvah <diakon.ng@gmail.com>
2017-04-27 15:34:05 +0100

    Fix support for multiple encodings

--- src/Observers/AutoHashidsObserver.php
+++ src/Observers/AutoHashidsObserver.php
@@ -12,9 +12,20 @@ class AutoHashidsObserver
     {
         $column = property_exists($model, "hashidsColumn") ? $model->hashidsColumn : "uid";
         $connection = property_exists($model, "hashidsConnection") ? $model->hashidsConnection : "main";
-        $encodings = property_exists($model, "hashidsEncodings") ? $model->hashidsEncodings : [$model->getKey()];
+        $encodings = property_exists($model, "hashidsEncodings") ? $model->hashidsEncodings : $model->getKey();
 
         $hashConn = Hashids::connection($connection);
+
+        if(is_array($encodings)) {
+            $en = [];
+            foreach ($encodings as $encoding) {
+                $en[] = $model->$encoding;
+            }
+            $encodings = $en;
+        } else {
+            $encodings = $model->$encodings;
+        }
+
         $model->$column = call_user_func_array([$hashConn, "encode"], $encodings);
 
         $model->save();
